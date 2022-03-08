09b2eb08b73131b24b449e05eee45fd2df555b19
bryancitu <bryanjesus.ra@gmail.com>
2022-03-08 02:19:59 -0500

    separando los schemas y resolvers en archivos separados y trabajando con schemas mas complejos

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3001
 
 // definiendo el esquema
-const schema = buildSchema(`
-    type Query {
-        hello: String
-        saludo: String
-    }
-`)
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
+)
 
-// Configurar los reolvers
-const resolvers = {
-    hello: () => {
-        return "Hola Mundo"
-    },
-    saludo: () => {
-        return "Hola a todos"
-    },
-}
-
-app.use('/api',graphqlHTTP({
-    schema,
-    rootValue: resolvers,
-    graphiql: true
+app.use('/api', graphqlHTTP({
+  schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`)
 })
 
 // Ejecutamos el query hello
 // graphql({
-//     schema: schema, 
+//     schema: schema,
 //     source: '{ hello, saludo }',
 //     rootValue: resolvers
 // }).then((data) => {
