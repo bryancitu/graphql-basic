d083f006b7f44ae432043150764d174124d46be7
bryancitu <bryanjesus.ra@gmail.com>
2022-03-08 01:44:11 -0500

    graphql with express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+
+const app = express()
+const port = process.env.port || 3001
 
 // definiendo el esquema
 const schema = buildSchema(`
@@ -13,18 +18,28 @@ const schema = buildSchema(`
 // Configurar los reolvers
 const resolvers = {
     hello: () => {
-        return 'Hola Mundo'
+        return "Hola Mundo"
     },
     saludo: () => {
-        return 'Hola a todos'
+        return "Hola a todos"
     },
 }
 
-// Ejecutamos el query hello
-graphql({
-    schema: schema, 
-    source: '{ hello, saludo }',
-    rootValue: resolvers
-}).then((data) => {
-    console.log(data);
+app.use('/api',graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
 })
+
+// Ejecutamos el query hello
+// graphql({
+//     schema: schema, 
+//     source: '{ hello, saludo }',
+//     rootValue: resolvers
+// }).then((data) => {
+//     console.log(data);
+// })
